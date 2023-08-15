101bb57da7510356d734856233c568fa58a81094
Justin <127259451+JustinWebDevs@users.noreply.github.com>
2023-08-15 18:00:51 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const textArea = document.getElementById('textArea');
+const btn = document.getElementById('btn');
+
+const formatData = (data) => {
+    
+}
+
+btn.addEventListener('click', ()=>{
+    formatData(textArea.value)
+});
