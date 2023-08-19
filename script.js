6ef7eb9f225adfa0303f7018bc08f3291e63ce4d
Justin <127259451+JustinWebDevs@users.noreply.github.com>
2023-08-19 11:30:57 -0500

    Json to SQL ready
    funciona el convertidor, pero tiene un fallo con los datos tipo null

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const textArea = document.getElementById('textArea');
+const textAreaResult = document.getElementById('textAreaResult');
+const table = document.getElementById('table');
 const btn = document.getElementById('btn');
 
-const formatData = (data) => {
+const formatData = (data, table) => {
     
-const expresionRegular = /\{([^}]+)\}/;
-const json = data.match(expresionRegular);
+  const regex = /\{(.*?)\}/g;
+  const resultArray = [];
+  let match;
+  
+  while ((match = regex.exec(data)) !== null) {
+    resultArray.push(match[0]);
+  }
+  
+  const jsonArray = [];
 
-if (json) {
-  const textoEntreLlaves = json[1];
-  console.log(textoEntreLlaves);
-} else {
-  console.log("No se encontraron llaves con texto.");
+  for (const item of resultArray) {
+    jsonArray.push(JSON.parse(item));
+  }
+
+  formatSql(jsonArray, table);
+
 }
 
+function formatSql(json, table){
+  let sql = ``;
+  for (const item of json) {
+    sql += `INSERT INTO ${table} (${Object.keys(item)}) VALUES (${Object.values(item)});\n`;
+  }
+  
+  textAreaResult.value = sql;
+} 
 
-}
 
 btn.addEventListener('click', ()=>{
-    formatData(textArea.value)
+    formatData(textArea.value, table.value)
 });
